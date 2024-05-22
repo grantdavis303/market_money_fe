0d87d66baed3e453be09e8e6a18168e816e35191
Grant Davis <grantdavis303@gmail.com>
2024-05-22 17:20:30 -0600

    Create Feature Test for US1

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -2,8 +2,24 @@ require 'rails_helper'
 
 RSpec.describe "Markets Index Page", type: :feature do
   it "lists all markets" do
+    json_response = File.read("spec/fixtures/all_markets.json")
+    stub_request(:get, "http://localhost:3000/api/v0/markets").to_return(status: 200, body: json_response)
+
+    json_response = File.read("spec/fixtures/single_market.json")
+    single = stub_request(:get, "http://localhost:3000/api/v0/markets/322458").to_return(status: 200, body: json_response)
+
+    visit markets_path
+
+    within ".all_markets" do
+      within ".im_322458" do
+        expect(page).to have_link("14&U Farmers' Market")
+        expect(page).to have_content("City: Washington")
+        expect(page).to have_content("State: District of Columbia")
+
+        click_on("14&U Farmers' Market")
+
+        expect(current_path).to eq("/markets/322458")
+      end
+    end
   end
 end
-
-# json_response = File.read("") # url to json file
-# stub_request(:get, "https://api.themoviedb.org/3/trending/movie/week?api_key=0f7ff543b9146c27bb69c85b227e5f63").to_return(status: 200, body: json_response)
