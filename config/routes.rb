f3c240813de8678a7e940b650d62df6e94a2a2e3
Grant Davis <grantdavis303@gmail.com>
2024-05-23 15:54:39 -0600

    Add Routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :markets
+  resources :markets, only: [:index, :show]
+  resources :vendors, only: [:show]
 end
